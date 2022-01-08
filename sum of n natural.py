4c080c43de9acc83035e52171c5f3514c47cb056
Kumar Arnav <78438898+Sniperx1077@users.noreply.github.com>
2022-01-09 00:32:14 +0530

    Update sum of n natural.py

--- sum of n natural.py
+++ sum of n natural.py
@@ -1,8 +1,13 @@
-def sum2(n):
-    return n*(n+1)//2
-def sum1(n):
-    sum=0
-    for i in range(1,n+1):
-        sum=sum+i
-    return sum
-        
+# Sum of natural numbers up to num
+
+num = 16
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
